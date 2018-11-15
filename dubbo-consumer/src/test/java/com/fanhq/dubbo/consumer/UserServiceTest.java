23e80c2e3fbcd4430172f8879550e9a7893b9cb7
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2018-11-15 14:08:07 +0800

    初始化

--- dubbo-consumer/src/test/java/com/fanhq/dubbo/consumer/UserServiceTest.java
+++ dubbo-consumer/src/test/java/com/fanhq/dubbo/consumer/UserServiceTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
-public class DubboTest {
+public class UserServiceTest {
 
     @Reference
     public IUserService userService;
